f99b6c7a5aa80c623ca7de6bfecd0cde5c0d42d3
Emi-ife Faluyi <emiifefaluyi@gmail.com>
2022-09-25 17:31:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let mark = 95;
+    if (95 > 90) {
+      console.log('AA');
+    }  else if (95 > 80 &&  95 <= 90) {
+      console.log('AB');
+    }
+      else if (95 > 70 && 95 <= 80) {
+        console.log('BB');
+      }
+      else if (95 > 60 && 95 <= 70) {
+        console.log('BC');
+      }
+      else if (95 > 50 && 95 <= 60) {
+        console.log('CC');
+      }
+      else if (95 > 40 && 95 <= 50) {
+        console.log('CD');
+      }
+      else if (95 > 30 && 95 <= 50) {
+        console.log('DD');
+      }
+      else {
+        console.log('FF');
+      }
